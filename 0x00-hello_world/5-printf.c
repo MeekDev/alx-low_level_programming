56074d779f9d63a0525cdfe8621dbe6015bcd9d3
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-10 20:45:35 -0800

    Check

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Some comments for the main function
+ * Goodluck
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
