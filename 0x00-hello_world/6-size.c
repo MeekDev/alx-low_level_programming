b51538fb3a1dc8cbc49a95a77b55cf2532e40558
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-10 22:26:01 -0800

    Check

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char c;
 float f;
 long li;
 long long lli;
-printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
-printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(lli));
-printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(li));
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(i));
+printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 return (0);
 }
