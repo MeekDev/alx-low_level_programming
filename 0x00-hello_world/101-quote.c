42f9a5fb8527edd636e7250525254007b9e05fa7
MeekDev <mrtech0x@gmail.com>
2022-03-10 17:44:02 -0800

    My first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,26 +2,22 @@
 
 #include <unistd.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - prints exactly "and that piece of art is useful"
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- *   * - Dora Korpar, 2015-10-19",
- *
- *    * followed by a new line, to the standard error.
- *
- *     * Return: Always 0 (Success)
- *
- *      */
+ * Return: Always 0 (Success)
+ */
+
+
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
+	return (1);
 
 }
