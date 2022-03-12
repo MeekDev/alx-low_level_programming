d76087d21c27103c52c129c57602b1aaee1a7c1a
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-12 13:49:33 -0800

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Prints a to z in small letters
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+return (0);
+}
