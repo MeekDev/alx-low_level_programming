cf402465d10ab68cd70beb3079f1b6536bfd99a7
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-15 15:46:53 -0700

    Done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_alphabet -  prints 10 times the alphabet
+ * print_alphabet_x10 -  prints 10 times the alphabet
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	
