6be052be917a78f942470521aa84a34d61405f80
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-15 13:01:04 -0700

    Check

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: prints _putchar, followed by a new line
+ */
+int main(void)
+{
+	int arr[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
+	int arrSize = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < arrSize; i++)
+	{
+		_putchar(arr[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
