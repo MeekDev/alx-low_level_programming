89a9885da4efaa2e1d958be8d7062973dddbb63a
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-15 22:01:53 -0700

    Done

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Fibonacci sequence
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	int fibonacci(int);
+
+	int fibonacci(int n)
+	{
+		if (n < 2)
+		{
+			return (1);
+		}
+		else
+		{
+			return (fibonacci(n - 1) + fibonacci(n - 2));
+		}
+	}
+
+	int i;
+
+	for (i = 0; i <= 50; i++)
+	{
+		printf("%d, ", fibonacci(i));
+	}
+	return (0);
+}
