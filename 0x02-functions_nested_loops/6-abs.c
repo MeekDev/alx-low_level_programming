faf9655090681d4ac36c5ba16c9ebd994302c4fd
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-15 17:36:53 -0700

    Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - absolute value of an integer
+ * @n: paramter in function _abs
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	int x = abs(n);
+
+	return (x);
+}
