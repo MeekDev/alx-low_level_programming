a574656edfc18de78b9895684c4ef2dc7b01296e
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-17 13:43:26 -0700

    Done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,2 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-/**
- * main - collation of function prototypes
- * Return: The values
- */
+int _putchar(char c);
 int _isupper(int c);
-int mul(int a, int b);
-#endif /* MAIN_H */i
