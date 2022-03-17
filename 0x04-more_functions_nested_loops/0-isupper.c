7c58f54f47d1a2697fc45a69fb98195d0fb0e8aa
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-17 05:57:14 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _isupper - output uppercase letter
+ * @c: parameter
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef main_h
+#define main_h
+/**
+ * main - collation of function prototypes
+ */
+int _isupper(int c);
+#endif;
