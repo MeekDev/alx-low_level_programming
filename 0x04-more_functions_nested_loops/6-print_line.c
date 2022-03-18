77d950fe5dd4cfef1257e850e964309c4fc553df
Kiisi-Felix-Destiny <kiisifelix06@gmail.com>
2022-03-17 20:20:53 -0700

    Done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,16 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n <= 0)
+	while (i < n)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		while (i <= n)
+		if (n > 0)
 		{
-			_putchar('_');
+			_putchar(95);
 			i++;
 		}
-		_putchar('\n');
+		else
+		{
+			_putchar('\n');
+		}
 	}
 }
