0c14225a75cb0d33fe8592661d275c2286919e2c
MeekDev <mrtech0x@gmail.com>
2022-03-27 12:58:53 -0700

    Update reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 #ifndef _MAIN_H_
 #define _MAIN_H_
-=======
 int _putchar(char c);
->>>>>>> 91bb81571e297ccc77413a9ad1ea3e1663afc9ed
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -14,9 +11,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-<<<<<<< HEAD
 int _atoi(char *s);
 int _putchar(char c);
 #endif
-=======
->>>>>>> 91bb81571e297ccc77413a9ad1ea3e1663afc9ed
